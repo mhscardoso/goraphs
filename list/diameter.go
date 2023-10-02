6f5aa0451f688fa3ce1e53a945013043de49e3b9
Matheus Cardoso <mhcardoso@poli.ufrj.br>
2023-10-02 08:46:19 -0300

    Adding tests to facilitate Case Studies

--- list/diameter.go
+++ list/diameter.go
@@ -1,8 +1,54 @@
 package list
 
+import (
+	"math/rand"
+)
+
 // All algorithms to the shortest path
 
 // func (L *List) BFSrn() {
 // 	rn := int(math.Sqrt(float64(L.N)))
 
 // }
+
+func (L *List) DiameterApprox() int {
+	first_vertex := rand.Intn(L.N)
+
+	_, _, level := L.BFS(first_vertex)
+
+	max_level := 0
+
+	for i := range level {
+		if level[i] > level[max_level] {
+			max_level = i
+		}
+	}
+
+	_, _, level2 := L.BFS(max_level + 1)
+
+	max_level = 0
+
+	for i := range level {
+		if level2[i] > level2[max_level] {
+			max_level = i
+		}
+	}
+
+	return int(level2[max_level])
+}
+
+func (L *List) Diameter() uint {
+	var max_distance uint = 0
+
+	for i := range L.Vector {
+		for j := i + 1; j < L.N; j++ {
+			dist := L.Distance(i+1, j)
+
+			if dist > int(max_distance) {
+				max_distance = uint(dist)
+			}
+		}
+	}
+
+	return max_distance
+}
--- list/search.go
+++ list/search.go
@@ -1,8 +1,6 @@
 package list
 
 import (
-	"math/rand"
-
 	"github.com/mhscardoso/goraphs/queue"
 	"github.com/mhscardoso/goraphs/stack"
 )
@@ -179,46 +177,4 @@ func (L *List) Distance(s int, d int) int {
 	}
 
 	return -1
-}
-
-func (L *List) DiameterApprox() int {
-	first_vertex := rand.Intn(L.N)
-
-	_, _, level := L.BFS(first_vertex)
-
-	max_level := 0
-
-	for i := range level {
-		if level[i] > level[max_level] {
-			max_level = i
-		}
-	}
-
-	_, _, level2 := L.BFS(max_level + 1)
-
-	max_level = 0
-
-	for i := range level {
-		if level2[i] > level2[max_level] {
-			max_level = i
-		}
-	}
-
-	return int(level2[max_level])
-}
-
-func (L *List) Diameter() uint {
-	var max_distance uint = 0
-
-	for i := range L.Vector {
-		for j := i + 1; j < L.N; j++ {
-			dist := L.Distance(i+1, j)
-
-			if dist > int(max_distance) {
-				max_distance = uint(dist)
-			}
-		}
-	}
-
-	return max_distance
 }
